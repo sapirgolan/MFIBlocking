7225f96badc224e41089d6e79570b6c7ab5a7d4f
Sapir Golan <sapirgolan@gmail.com>
2014-01-04 14:57:09 +0200

    Extracted some code to private methods

--- meta-blocking-converter/converter/src/main/java/il/ac/technion/ie/converter/Convertor.java
+++ meta-blocking-converter/converter/src/main/java/il/ac/technion/ie/converter/Convertor.java
@@ -4,6 +4,7 @@ import il.ac.technion.ie.metablocking.Attribute;
 import il.ac.technion.ie.metablocking.EntityProfile;
 import il.ac.technion.ie.utils.SerializationUtilities;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -17,9 +18,9 @@ public class Convertor {
 
 	private static final String DELIMITER_VALUES_IN_FIELD = " ";
 
-	public List<String> extractFieldsNames(final ArrayList<EntityProfile> entityProfiles) {
+	public List<String> extractFieldsNames(final List<EntityProfile> list) {
 		ArrayList<String> result = new ArrayList<String>();
-		for (EntityProfile entityProfile : entityProfiles) {
+		for (EntityProfile entityProfile : list) {
 			HashSet<Attribute> attributes = entityProfile.getAttributes();
 			for (Attribute attribute : attributes) {
 				String name = attribute.getName();
@@ -32,36 +33,56 @@ public class Convertor {
 	}
 	
 	//TODO: change to map of Map<String, StringBuilder> 
-	public List<Map<String, String>> extractValues( final ArrayList<EntityProfile> entityProfiles ) {
+	public List<Map<String, String>> extractValues( final List<EntityProfile> entityProfiles ) {
 		ArrayList<Map<String, String>> result = new ArrayList<Map<String, String>>(entityProfiles.size());
 		List<String> fieldsNames = extractFieldsNames(entityProfiles);
 		
 		for (EntityProfile entityProfile : entityProfiles) {
-			HashMap<String, String> hashMap = new HashMap<String, String>();
-			for (String fieldsName : fieldsNames) {
-				hashMap.put(fieldsName, null);
-			}
+			HashMap<String, String> hashMap = initMap(fieldsNames);
 			result.add(hashMap);
 			
 			HashSet<Attribute> attributes = entityProfile.getAttributes();
 			for (Attribute attribute : attributes) {
 				String value = hashMap.get( attribute.getName() );
-				if (value != null) {
-					value = value + DELIMITER_VALUES_IN_FIELD + attribute.getValue();
-				} else {
-					value = attribute.getValue();
-				}
+				value = buildValue(attribute, value);
 				hashMap.put(attribute.getName(), value);
 			}
 		}
 		return result;
 	}
+
+	private String buildValue(Attribute attribute, String value) {
+		if (value != null) {
+			value = value + DELIMITER_VALUES_IN_FIELD + attribute.getValue();
+		} else {
+			value = attribute.getValue();
+		}
+		return value;
+	}
+
+	/**
+	 * Init a Map. Adds to it keys whose value are the content of fieldsNames parameter. The values of those new
+	 * entries is null.
+	 * @param fieldsNames
+	 * @return
+	 */
+	private HashMap<String, String> initMap(List<String> fieldsNames) {
+		HashMap<String, String> hashMap = new HashMap<String, String>();
+		for (String fieldsName : fieldsNames) {
+			hashMap.put(fieldsName, null);
+		}
+		return hashMap;
+	}
 	
 	@SuppressWarnings("unchecked")
 	public ArrayList<EntityProfile> loadEntityProfile(String filePath) {
 		
-		ArrayList<EntityProfile> profileList = (ArrayList<EntityProfile>) SerializationUtilities.loadSerializedObject(filePath);
-		return profileList;
+		File file = new File(filePath);
+		if (file.exists()) {
+			ArrayList<EntityProfile> profileList = (ArrayList<EntityProfile>) SerializationUtilities.loadSerializedObject(filePath);
+			return profileList;
+		}
+		return null;
 	}
 	
 	public void doAlgorithm(String filePath) {

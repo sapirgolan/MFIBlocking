eefbe52b8b4784b1f18c5df404f679b300980c99
Sapir Golan <sapirgolan@gmail.com>
2014-02-18 09:17:41 +0200

    Changed compilation from java 6 to java 7. added a new dependency

--- MFIblocks/env.bat
+++ MFIblocks/env.bat
@@ -1,1 +1,23 @@
-set PATH=%PATH%;C:\apache-maven-3.1.1\bin
+@echo off
+IF EXIST C:\Apache-maven-3.1.1 (
+@echo Apache-maven-3.1.1 exists under C drive.
+@set M2_HOME=C:\Apache-maven-3.1.1
+goto addMvnToPath
+) 
+IF EXIST C:\worksapce\Apache-maven-3.1.1 (
+@echo Apache-maven-3.1.1 exists under C:\workspace drive.
+@set M2_HOME=C:\worksapce\Apache-maven-3.1.1
+goto addMvnToPath
+)
+
+echo.
+echo ERROR: Apache-maven not found in your environment.
+echo Please add Apache-maven to your ststem and place it either 
+echo at "C:" or "C:\worksapce"
+echo.
+
+:addMvnToPath
+@set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_51
+
+@set PATH=%PATH%;%M2_HOME%\bin;
+@echo Apache Maven is ready for compilation!

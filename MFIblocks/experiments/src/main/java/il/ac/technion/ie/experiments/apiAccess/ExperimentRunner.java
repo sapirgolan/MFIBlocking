6a717e4b88e896033965852b438d3b07668eab61
Sapir Golan <s.golan@sap.com>
2015-10-10 15:58:25 +0300

    Added utility methods for experiments & change the execution of experiments

--- MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/apiAccess/ExperimentRunner.java
+++ MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/apiAccess/ExperimentRunner.java
@@ -1,20 +1,34 @@
 package il.ac.technion.ie.experiments.apiAccess;
 
 import il.ac.technion.ie.experiments.Utils.ExpFileUtils;
+import il.ac.technion.ie.experiments.exception.NoValueExistsException;
+import il.ac.technion.ie.experiments.exception.OSNotSupportedException;
+import il.ac.technion.ie.experiments.exception.SizeNotEqualException;
 import il.ac.technion.ie.experiments.model.BlockWithData;
-import il.ac.technion.ie.experiments.service.ExprimentsService;
-import il.ac.technion.ie.experiments.service.ParsingService;
-import il.ac.technion.ie.experiments.service.ProbabilityService;
+import il.ac.technion.ie.experiments.model.ConvexBPContext;
+import il.ac.technion.ie.experiments.model.UaiVariableContext;
+import il.ac.technion.ie.experiments.parsers.UaiBuilder;
+import il.ac.technion.ie.experiments.service.*;
+import il.ac.technion.ie.experiments.threads.CommandExacter;
 import il.ac.technion.ie.measurements.service.MeasurService;
 import il.ac.technion.ie.measurements.service.iMeasurService;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by I062070 on 27/08/2015.
  */
 public class ExperimentRunner {
 
+    static final Logger logger = Logger.getLogger(ExperimentRunner.class);
+
+    private final FuzzyService fuzzyService;
     private ParsingService parsingService;
     private ProbabilityService probabilityService;
     private iMeasurService measurService;
@@ -25,6 +39,7 @@ public class ExperimentRunner {
         probabilityService = new ProbabilityService();
         measurService = new MeasurService();
         exprimentsService = new ExprimentsService();
+        fuzzyService = new FuzzyService();
     }
 
     public static void main(String[] args) {
@@ -34,10 +49,11 @@ public class ExperimentRunner {
         }
         String datasetFile = args[0];
         ExperimentRunner experimentRunner = new ExperimentRunner();
-        experimentRunner.runExp(datasetFile);
+//        experimentRunner.runSimpleExp(datasetFile);
+        experimentRunner.runExperiments(datasetFile);
     }
 
-    public void runExp(String datasetPath) {
+    public void runSimpleExp(String datasetPath) {
         List<BlockWithData> blockWithDatas = parsingService.parseDataset(datasetPath);
         probabilityService.calcProbabilitiesOfRecords(blockWithDatas);
         double rankedValue = measurService.calcRankedValue(blockWithDatas);
@@ -53,5 +69,50 @@ public class ExperimentRunner {
             System.out.print("Total of " + filteredBlocks.size() + " blocks representative is wrong. ");
             System.out.println("output file can be found at: " + outputFilePath);
         }
+    }
+
+    public void runExperiments(String pathToDatasetFile) {
+        final List<BlockWithData> blockWithDatas = parsingService.parseDataset(pathToDatasetFile);
+        final Map<Integer, Double> splitProbabilityForBlocks = exprimentsService.sampleSplitProbabilityForBlocks(blockWithDatas);
+        List<Double> thresholds = exprimentsService.getThresholdSorted(splitProbabilityForBlocks.values());
+
+        CommandExacter commandExacter = new CommandExacter();
+        logger.info("Will execute experiments on following split thresholds: " + StringUtils.join(thresholds, ','));
+        for (Double threshold : thresholds) {
+            logger.info("Executing experiment with threshold " + threshold);
+            try {
+                logger.debug("splitting blocks");
+                List<BlockWithData> splitedBlocks = fuzzyService.splitBlocks(blockWithDatas, splitProbabilityForBlocks, threshold);
+                logger.debug("calculating probabilities on blocks after they were split");
+                probabilityService.calcProbabilitiesOfRecords(splitedBlocks);
+                UaiBuilder uaiBuilder = new UaiBuilder(splitedBlocks);
+                logger.debug("creating UAI file");
+                UaiVariableContext uaiVariableContext = uaiBuilder.createUaiContext();
+                logger.debug("UAI file was created at: " + uaiVariableContext.getUaiFile().getAbsoluteFile());
+                ConvexBPContext convexBPContext = exprimentsService.createConvexBPContext(uaiVariableContext);
+                //critical section - cannot be multi-thread
+                File outputFile = commandExacter.execute(convexBPContext);
+                if (outputFile.exists()) {
+                    logger.debug("Binary output of convexBP was created on: " + outputFile.getAbsolutePath());
+                    UaiConsumer uaiConsumer = new UaiConsumer(uaiVariableContext, outputFile);
+                    uaiConsumer.consumePotentials();
+                    FileUtils.forceDeleteOnExit(outputFile);
+                    logger.debug("Applying new probabilities on blocks");
+                    uaiConsumer.applyNewProbabilities(splitedBlocks);
+                }
+            } catch (SizeNotEqualException e) {
+                logger.error("Failed to split blocks since #blocs<>#splitProbabilities", e);
+            } catch (IOException e) {
+                logger.error("Cannot create context for ConvexBP algorithm", e);
+            } catch (InterruptedException e) {
+                logger.error("Failed to wait till the execution of ConvexBP algorithm has finished", e);
+                e.printStackTrace();
+            } catch (OSNotSupportedException e) {
+                logger.error("Cannot run ConvexBP algorithm on current machine", e);
+            } catch (NoValueExistsException e) {
+                logger.error("Failed to consume new probabilities", e);
+            }
+        }
+
     }
 }
--- MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/service/ExprimentsService.java
+++ MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/service/ExprimentsService.java
@@ -1,15 +1,23 @@
 package il.ac.technion.ie.experiments.service;
 
 import il.ac.technion.ie.experiments.model.BlockWithData;
+import il.ac.technion.ie.experiments.model.ConvexBPContext;
+import il.ac.technion.ie.experiments.model.UaiVariableContext;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.math3.distribution.UniformRealDistribution;
+import org.apache.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by I062070 on 27/08/2015.
  */
 public class ExprimentsService {
+
+    static final Logger logger = Logger.getLogger(ExprimentsService.class);
 
     /**
      * The method filters the blocks whose chosen representative is not the True Representative of the block.
@@ -26,5 +34,49 @@ public class ExprimentsService {
             }
         }
         return filteredBlocks;
+    }
+
+    /**
+     * The method return a MAP with the split probability of each block.
+     * The split probability is sampled from Uniform Real Distribution {@link UniformRealDistribution} Uniform distribution~[0,1]
+     *
+     * @param blocks - a Collection of blocks
+     * @return Map<Integer, Double>
+     */
+    public final Map<Integer, Double> sampleSplitProbabilityForBlocks(final Collection<BlockWithData> blocks) {
+        UniformRealDistribution uniformRealDistribution = new UniformRealDistribution();
+        Map<Integer, Double> splitProbMap = new HashMap<>();
+        for (BlockWithData block : blocks) {
+            splitProbMap.put(block.getId(), uniformRealDistribution.sample());
+        }
+        return splitProbMap;
+    }
+
+    public List<Double> getThresholdSorted(Collection<Double> values) {
+        List<Double> thresholds = new ArrayList<>(values);
+        Collections.sort(thresholds);
+        thresholds.remove(0);
+        return thresholds;
+    }
+
+    public ConvexBPContext createConvexBPContext(UaiVariableContext context) throws IOException {
+        final String DCBP_DIR = "C:\\Users\\i062070\\Downloads\\dcBP\\x64\\Debug";
+        File uaiVariableContextFile = context.getUaiFile();
+        if (uaiVariableContextFile == null || !uaiVariableContextFile.exists()) {
+            throw new FileNotFoundException("uaiVariableContextFile doesn't exist in given UaiVariableContext");
+        }
+        try {
+            FileUtils.copyFileToDirectory(uaiVariableContextFile, new File(DCBP_DIR));
+        } catch (IOException e) {
+            logger.error("Failed to copy uaiVariableContextFile to " + DCBP_DIR, e);
+            throw e;
+        }
+        String uaiFileName = uaiVariableContextFile.getName();
+        logger.trace("uaiFileName is: " + uaiFileName);
+        String outputFileName = "Out_" + System.nanoTime() + ".txt";
+        logger.trace("outputFileName for binary file is: " + outputFileName);
+        int waitIntervalInSeconds = 3;
+        logger.debug("Create ConvexBPContext with waitIntervalInSeconds of " + waitIntervalInSeconds);
+        return new ConvexBPContext(DCBP_DIR, uaiFileName, outputFileName, waitIntervalInSeconds);
     }
 }

c99ee8bfeb08a823dbfcef2de3ce0ba0d7fb538d
Sapir Golan <s.golan@sap.com>
2015-10-10 15:59:59 +0300

    The command the call convexBP will run the algorithm up to 10  times if it fails

--- MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/threads/CommandExacter.java
+++ MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/threads/CommandExacter.java
@@ -16,38 +16,52 @@ public class CommandExacter {
 
     static final Logger logger = Logger.getLogger(CommandExacter.class);
 
-    public void execute(ConvexBPContext context) throws IOException, OSNotSupportedException, InterruptedException {
+    public File execute(ConvexBPContext context) throws IOException, OSNotSupportedException, InterruptedException {
+        File outputFile;
+        int numberOfExecutions = 1;
         checkOS();
-
         Runtime runtime = Runtime.getRuntime();
-
         String command = createCommand(context);
-        logger.debug(String.format("Executing read UAI file by command: '%s'", command));
 
-        long startTime = System.nanoTime();
-        Process process = runtime.exec(command, new String[]{}, new File(context.getDir()));
-        long afterExecTime = System.currentTimeMillis();
-        Thread thread = new Thread(new ProcessTimeoutThread(process, afterExecTime, context.getWaitInterval()));
-        thread.start();
+        do {
+            logger.info(String.format("Executing convexBP algorithm for #%d time", numberOfExecutions));
+            logger.debug(String.format("Executing read UAI file by command: '%s'", command));
 
-        // any error message?
-        StreamGobbler errorGobbler = new StreamGobbler(process.getErrorStream(), StreamGobbler.ChanelType.ERROR);
-        // any output?
-        StreamGobbler outputGobbler = new StreamGobbler(process.getInputStream(), StreamGobbler.ChanelType.OUTPUT);
+            long startTime = System.nanoTime();
+            Process process = runtime.exec(command, new String[]{}, new File(context.getDir()));
+            long afterExecTime = System.currentTimeMillis();
+            Thread thread = new Thread(new ProcessTimeoutThread(process, afterExecTime, context.getWaitInterval()));
+            thread.start();
 
-        // kick them off
-        errorGobbler.start();
-        outputGobbler.start();
+            // any error message?
+            StreamGobbler errorGobbler = new StreamGobbler(process.getErrorStream(), StreamGobbler.ChanelType.ERROR);
+            // any output?
+            StreamGobbler outputGobbler = new StreamGobbler(process.getInputStream(), StreamGobbler.ChanelType.OUTPUT);
 
-        // any error???
-        // wait, if necessary, until the process object has terminated
-        logger.debug("Waiting if needed for previous read job to finish");
-        long waitTime = System.nanoTime();
-        int exitVal = process.waitFor();
-        long endTime = System.nanoTime();
-        logger.info("Total execution time: " + TimeUnit.NANOSECONDS.toSeconds(endTime - startTime));
-        logger.info("Total wait time time: " + TimeUnit.NANOSECONDS.toSeconds(endTime - waitTime));
-        logger.debug("ExitValue: " + exitVal);
+            // kick them off
+            errorGobbler.start();
+            outputGobbler.start();
+
+            // wait, if necessary, until the process object has terminated
+            logger.debug("Waiting if needed for previous read job to finish");
+            long waitTime = System.nanoTime();
+            int exitVal = process.waitFor();
+            long endTime = System.nanoTime();
+            logger.info(String.format("Total execution & waitTime of convexBP: %d, %d",
+                    TimeUnit.NANOSECONDS.toSeconds(endTime - startTime), TimeUnit.NANOSECONDS.toSeconds(endTime - waitTime)));
+            logger.debug("ExitValue: " + exitVal);
+            outputFile = new File(context.getPathToOutputFile());
+            numberOfExecutions++;
+        } while (keepJobRunning(outputFile, numberOfExecutions));
+        return outputFile;
+    }
+
+    private boolean keepJobRunning(File outputFile, int numberOfExecutions) {
+        if ((outputFile == null || !outputFile.exists()) && numberOfExecutions < 11) {
+            return true;
+        }
+        logger.error("Failed to run convexBP for 10 times");
+        return false;
     }
 
     private String createCommand(ConvexBPContext context) {

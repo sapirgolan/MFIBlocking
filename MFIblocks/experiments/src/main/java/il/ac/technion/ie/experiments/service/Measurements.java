ab87bff0d577481fbd99d1edde86ccb2108a86f7
Sapir Golan <s.golan@sap.com>
2015-12-30 16:33:11 +0200

    add missing 'space'

--- MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/service/Measurements.java
+++ MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/service/Measurements.java
@@ -132,8 +132,8 @@ public class Measurements implements IMeasurements {
     @Override
     public DuplicateReductionContext representativesDuplicateElimination(
             Multimap<Record, BlockWithData> duplicates, Multimap<Record, BlockWithData> cleaned, int cleanBlocksSize) {
-        logger.info("In blocks that were created by Canopy and probs calculated by Miller, there are " + duplicates.keySet().size() + "unique representatives.");
-        logger.info("In blocks that were created by Canopy and probs calculated by ConvexBP, there are " + cleaned.keySet().size() + "unique representatives.");
+        logger.info("In blocks that were created by Canopy and probs calculated by Miller, there are " + duplicates.keySet().size() + " unique representatives.");
+        logger.info("In blocks that were created by Canopy and probs calculated by ConvexBP, there are " + cleaned.keySet().size() + " unique representatives.");
         if (logger.isTraceEnabled()) {
             writeToLogInfo(duplicates);
             writeToLogInfo(cleaned);

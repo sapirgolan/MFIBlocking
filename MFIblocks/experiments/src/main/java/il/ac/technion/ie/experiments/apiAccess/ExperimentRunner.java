9d5d210efee6c8e59d4b85343c07eee8bb925033
Sapir Golan <s.golan@sap.com>
2016-02-22 08:48:18 +0200

    Added Profiling mode Added Logging of duration Logger set to ERROR

--- MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/apiAccess/ExperimentRunner.java
+++ MFIblocks/experiments/src/main/java/il/ac/technion/ie/experiments/apiAccess/ExperimentRunner.java
@@ -6,6 +6,8 @@ import il.ac.technion.ie.experiments.experimentRunners.AbstractExperiment;
 import il.ac.technion.ie.experiments.experimentRunners.CreateCanopies;
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
+
 /**
  * Created by I062070 on 27/08/2015.
  */
@@ -18,12 +20,21 @@ public class ExperimentRunner {
 
         AbstractExperiment experiment = new CreateCanopies();
         logger.info("Starting an experiment");
-
+        if (context.isProfilingMode()) {
+            logger.info("Entering Profiling mode");
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        long startTime = System.nanoTime();
         try {
             experiment.runExperiments(context.getPathToDataset());
         } catch (Throwable e) {
             logger.error("There was an exception!", e);
         }
+        TimeLogger.logDurationInSeconds(startTime, "Creating all canopies took");
         System.exit(0);
     }
 }
